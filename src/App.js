45f00cec98409c2689548b57ea71b68b3acdec62
Blake <bpro0821@gmail.com>
2019-07-15 16:14:04 -0500

    through step 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-//TODO: STEP 1 - Import the useState hook.
+yar//TODO: STEP 1 - Import the useState hook.
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [homeScore, setHomeScore] = useState(32)
+  const [awayScore, setAwayScore] = useState(32)
 
+  const homeTouchdown = ()  => {
+    setHomeScore(homeScore + 7)
+  }
+  const homeFieldGoal = () => {
+    setHomeScore (homeScore + 3)
+  }
+  const awayTouchdown () => {
+    setAwayScore (awayScore + 7)
+  }
+  const awayFieldGoal = () => {
+    setAwayScore(awayScore + 3)
+  }
   return (
     <div className="container">
       <section className="scoreboard">
